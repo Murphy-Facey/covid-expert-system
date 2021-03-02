9e9404666490705fe3f066d26326fdce75d11ac1
Murphy-Facey <55172328+Murphy-Facey@users.noreply.github.com>
2021-03-02 08:18:41 -0500

    added rough rules and facts

--- ces-knowledge-base.pl
+++ ces-knowledge-base.pl
@@ -1,38 +1,68 @@
-% covid diagnosis system
+% this allows us to change the values in the 
+% statistic predicate
+:- dynamic stats/3.
 
-go :- hypothesize(Covid),
-        write('I think you '),
-        write(Covid), nl, undo.
+% 1st represents the number of persons diagnose
+% 2nd represents the number of persons with mild symptoms
+% 3rd represents the number of persons with severe symptoms
+stats(0,0,0).
 
-/* HYPOTHOSES TO BE TESTED */
-hypothesize('have covid') :- covid, !.
-hypothesize('do not have covid').
+symptom("low blood pressure", "fainting").
+symptom("unknown", "fainting").
+
+symptom(fever).
+lowBloodSym(fever).
+
+get_temperature(FahrTemp):-
+    % not going to included the question because 
+    % it is handled by the UI
+    read(CelcTemp),
+    FahrTemp is CelcTemp * 9 / 5 + 32.
+
+fever(Effect, FahrTemp):-
+    (FahrTemp >= 100.4 -> Effect is 1; Effect is 0).
+
+% mild: 2, severe: 2 -> 
+% mild: 2, severe: 2
+% mild: 3, severe: 1
+% mild: 1, severe: 1
 
 
-/* -- RULES FOR DIAGNOSING COVID -- */
-covid :-
-verify(fever),
-verify('dry cough'),
-verify('shortness of breath'),
-write('Advices and Suggestions'),
-nl,
-write('Quarantine for two weeks'),
-nl.
+% if there is two mild symptoms, the number of severe symptoms 
+% are less than the number of mild symptoms then mild symptoms.
+% else if severe symptoms are two or more than mild when mild
+% is two, then severe
+% else nothing
 
-/* -- RULES ASKING ABOUT THE SYMPTOMS -- */
-ask(Symptom) :-
-write('Do the patient have the following symptoms: '),
-write(Symptom), write('? '),
-read(Response), nl,
-( (Response == yes; Response == y) ->
-    assert(yes(Symptom));
-    assert(no(Symptom)), fail).
+covid:-
+    get_temperature(FahrTemp),
+    fever(FeverEffect, FahrTemp),
+	(FeverEffect == 1 -> write('Has fever'); write('does not have fever')).
 
-:- dynamic yes/1,no/1.
+lowBloodSym(low_blood_pressure).
+symp(low_blood_pressure).
 
-verify(S) :- (yes(S) -> true; (no(S) -> fail; ask(S))).
+low_blood(Sym1, Sym2, Sym3, Effect) :-
+    (lowBloodSym(Sym1); lowBloodSym(Sym2); lowBloodSym(Sym3) -> 
+    Effect is 1; Effect is 0).
 
-/* -- UNDO ALL ASSERTIONS OF YES AND NO -- */
-undo :- retract(yes(_)), fail.
-undo :- retract(no(_)), fail.
-undo.
+/*low_blood_pressure(...):-
+    symptom(DizzyEffect, Symp1),
+    symptom(FaintEffect, "fainting"),
+    symptom(BlurEffect, "blurry vision"),
+    TotalEffect is DizzyEffect + BlurEffect + FaintEffect,
+    (TotalEffect > 1 -> check_dia_sys_level; fail).*/
+
+check_dia_sys_level:-
+    check_diastolic_reading(DiaEffect),
+    check_systolic_reading(SysEffect),
+    ((DiaEffect == 1, SysEffect == 1) -> write("might have low blood pressure");write("don't have low blood pressure")).
+
+check_diastolic_reading(Effect):-
+    read(
+    
+    
+symptom(Effect, Symptom):-
+    write(Symptom), write(" ?"),
+    read(Response),
+    ((Response == yes; Response == y) -> Effect is 1; Effect is 0)).
