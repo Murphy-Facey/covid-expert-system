457ec2ae8000187bc7f4f8510c4d733b9de4385d
Murphy-Facey <55172328+Murphy-Facey@users.noreply.github.com>
2021-03-15 09:06:22 -0500

    updated the UI and functionality

--- ui/js/hypotension.js
+++ ui/js/hypotension.js
@@ -1,7 +1,11 @@
 window.onload = function () {
-    eel.get_symptoms("low_blood_pressure")(addSymptoms);
+    eel.get_symptoms("low_blood_pressure")(add_symptoms);
 }
 
-function addSymptoms(symptoms) {
+function add_symptoms(symptoms) {
     console.log(symptoms);
+}
+
+function go_back() {
+    document.location.href = 'http://localhost:8000/index.html';
 }
